98c821567d9e3edab7ca45c53a9f964cb5e4fe10
Andreferraz20 <luizlopesferraz22@gmail.com>
2019-10-16 09:28:17 -0300

    Projeto imprimindo pecas

---
+++ src/aplication/Program.java
@@ -0,0 +1,14 @@
+package aplication;
+
+import boardgame.Board;
+import chess.ChessMatch;
+
+public class Program {
+    public static void main(String[] args) {
+
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+
+    }
+
+}
---
+++ src/chess/ChessException.java
@@ -0,0 +1,10 @@
+package chess;
+
+public class ChessException extends RuntimeException {
+    private static final long serialVersionID = 1L;
+
+    public ChessException(String msg){
+        super(msg);
+    }
+
+}
